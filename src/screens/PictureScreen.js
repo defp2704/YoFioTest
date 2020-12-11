3be043decc025822f9978222f81447763d99f7cc
Eduardo Ferreira <dferreira@arkusnexus.com>
2020-12-11 15:08:25 -0800

    design pictures screen

--- src/screens/PictureScreen.js
+++ src/screens/PictureScreen.js
@@ -1,9 +1,21 @@
 import React, { useEffect } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet
+} from 'react-native';
+import { widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import { 
+  useSelector, 
+  useDispatch
+} from 'react-redux';
+
 import {
   onChangesTab,
 } from '../actions/navigationActions';
+import FileComponent from '../components/FileComponent';
 
 export default function PictureScreen({
   navigation,
@@ -31,10 +43,49 @@ export default function PictureScreen({
     }
   }, [navigation, goBackPress, screen, dispatch]);
 
+  const renderImage = ({ item: itemPath }) => itemPath !== '' && (
+    <View style={styles.containerImage}>
+      <FileComponent itemPath={itemPath} />
+    </View>
+  );
+
+  const getKeyItem = (_, index) => index.toString();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-      <ActivityIndicator animating={isLoading} color='black' />
-      <Text>Pictures: {images.length} </Text>
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator
+          animating={isLoading}
+          color='black'
+        />
+      ) : (
+          <FlatList
+            data={images}
+            extraData={images}
+            renderItem={renderImage}
+            keyExtractor={getKeyItem}
+            numColumns={2}
+          /> 
+        )
+      }
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
+  containerImage: {
+    width: wp('45%'),
+    height: wp('100%') ,
+    margin: wp('2%'),
+    borderRadius: wp('4%'),
+    overflow: 'hidden',
+  }
+});
+
